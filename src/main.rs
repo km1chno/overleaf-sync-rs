516292e20d805090912116f3ec95a645b2a3171f
katzper01 <katzper.michno@gmail.com>
2024-06-26 16:46:39 +0200

    search for .olsync upwards in directory hierarchy

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ pub mod overleaf_client;
 
 use crate::{
     login_browser::launch_login_browser,
-    overleaf_client::{OverleafClient, ProjectsList, SessionCookie},
+    overleaf_client::{OverleafClient, SessionCookie},
 };
 
-use anyhow::{bail, Result};
+use anyhow::{bail, Context, Result};
 use chrono::Utc;
 use clap::{Parser, Subcommand};
-use std::io::Cursor;
+use std::{
+    env::{self},
+    io::Cursor,
+};
 use std::{
     fs::{self, File},
     io::BufReader,
@@ -29,7 +32,7 @@ enum Commands {
         #[arg(short, long)]
         name: String,
     },
-    List,
+    Pull,
 }
 
 #[tokio::main]
@@ -41,14 +44,8 @@ async fn main() -> Result<()> {
             Ok(()) => println!("Successfully cloned project {name}."),
             Err(err) => eprintln!("{err}\nFailed to clone project {name}."),
         },
-        Some(Commands::List) => match list_action().await {
-            Ok(projects) => {
-                projects
-                    .projects
-                    .into_iter()
-                    .for_each(|project| println!("{}: {}", project.id, project.name));
-            }
-            Err(err) => eprintln!("{err}\nFailed to list projects."),
+        Some(Commands::Pull) => match pull_action().await {
+            _ => {}
         },
         None => {}
     }
@@ -56,25 +53,47 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
+// Returns .olsync directory in current repository. It traverses directory tree starting from
+// currect directory and going upwards.
+fn get_olsync_directory() -> Option<PathBuf> {
+    let mut current_dir = env::current_dir().ok();
+
+    while let Some(pb) = current_dir {
+        let dir_str = match pb.to_str() {
+            Some(s) => format!("{s}/.olsync"),
+            _ => return None,
+        };
+
+        if matches!(fs::exists(dir_str.clone()), Ok(true)) {
+            return Some(PathBuf::from(dir_str));
+        }
+
+        current_dir = pb.parent().map(PathBuf::from);
+    }
+
+    None
+}
+
 // Check whether .olsync directory exists.
 fn is_ols_repository() -> bool {
-    matches!(fs::exists(".olsync"), Ok(true))
+    get_olsync_directory().is_some()
 }
 
 // Initialize empty .olsync directory.
-fn init_ols_repository() -> Result<()> {
+fn init_ols_repository(project_name: &String) -> Result<()> {
     if is_ols_repository() {
         bail!("Already an olsync repository.");
     }
 
     fs::create_dir(".olsync")?;
+    fs::write(".olsync/name", project_name)?;
 
     Ok(())
 }
 
 // Try to retrieve cached session cookie from .olsync/olauth.
-fn get_session_cookie_from_file() -> Option<SessionCookie> {
-    let file = File::open(".olsync/olauth");
+fn get_session_cookie_from_file(olsync_dir: &PathBuf) -> Option<SessionCookie> {
+    let file = File::open(olsync_dir);
 
     match file {
         Ok(f) => {
@@ -89,34 +108,23 @@ fn get_session_cookie_from_file() -> Option<SessionCookie> {
 }
 
 // Save session cookie to .olsync/olauth.
-fn save_session_cookie_to_file(cookie: &SessionCookie) -> Result<()> {
+fn save_session_cookie_to_file(cookie: &SessionCookie, olsync_dir: &PathBuf) -> Result<()> {
     let serialized_cookie = serde_json::to_string(cookie)?;
-    fs::write(".olsync/olauth", serialized_cookie)
+    fs::write(olsync_dir, serialized_cookie)
         .or_else(|_| bail!("Failed to save session cookie to .olsync/olauth"))
 }
 
 // Read cached session cookie or spawn browser to login and
 // save the new cookie in .olsync/olauth.
-fn get_session_cookie() -> Result<SessionCookie> {
-    if !is_ols_repository() {
-        init_ols_repository()?;
-    }
-
-    match get_session_cookie_from_file() {
+fn get_session_cookie(olsync_dir: &PathBuf) -> Result<SessionCookie> {
+    match get_session_cookie_from_file(olsync_dir) {
         Some(cookie) => Ok(cookie),
         _ => {
             let cookie = launch_login_browser()?;
-            save_session_cookie_to_file(&cookie)?;
+            save_session_cookie_to_file(&cookie, olsync_dir)?;
             Ok(cookie)
         }
     }
-}
-
-// List all remote projects.
-async fn list_action() -> Result<ProjectsList> {
-    let session_cookie = get_session_cookie()?;
-    let overleaf_client = OverleafClient::new(session_cookie);
-    overleaf_client.get_all_projects().await
 }
 
 // Clone project into current directory.
@@ -128,9 +136,11 @@ async fn clone_action(name: &String) -> Result<()> {
         ));
     }
 
-    init_ols_repository()?;
+    init_ols_repository(name)?;
 
-    let session_cookie = get_session_cookie()?;
+    let olsync_dir = get_olsync_directory().with_context(|| "Failed to find .olsync directory.")?;
+
+    let session_cookie = get_session_cookie(&olsync_dir)?;
     let overleaf_client = OverleafClient::new(session_cookie);
     let project = overleaf_client.get_project(name).await?;
 
@@ -139,6 +149,11 @@ async fn clone_action(name: &String) -> Result<()> {
         .await?
         .to_vec();
 
-    zip_extract::extract(Cursor::new(archive), &PathBuf::from("."), true)
+    zip_extract::extract(Cursor::new(archive), &PathBuf::from(name), true)
         .or_else(|_| bail!("Failed to extract downloaded project zip file."))
+}
+
+// Pull the current state from Overleaf.
+async fn pull_action() -> Result<()> {
+    Ok(())
 }
