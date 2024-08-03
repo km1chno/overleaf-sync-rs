82c61c35eebee2f754f21a6907dc40b757f35d75
katzper01 <katzper.michno@gmail.com>
2024-08-03 12:00:05 +0200

    spinners!

--- src/custom_log.rs
+++ src/custom_log.rs
@@ -1,7 +1,20 @@
 use colored::{ColoredString, Colorize};
 use env_logger::fmt::Formatter;
 use log::{Level, Record};
+use spinoff::{spinners, Color, Spinner};
 use std::io::Write;
+
+fn with_prefix(msg: String) -> String {
+    msg.replace("\n", "\n |  ")
+}
+
+pub fn success_message(msg: String) -> String {
+    format!("{} {}", "[✔]".green(), with_prefix(msg).green())
+}
+
+fn error_message(msg: String) -> String {
+    format!("{} {}", "[!]".red(), with_prefix(msg).red())
+}
 
 pub fn custom_log_format(buf: &mut Formatter, record: &Record) -> std::io::Result<()> {
     let level = record.level();
@@ -13,7 +26,7 @@ pub fn custom_log_format(buf: &mut Formatter, record: &Record) -> std::io::Resul
         _ => ColoredString::from(""),
     };
 
-    let msg_with_prefixes: String = record.args().to_string().replace("\n", "\n | ");
+    let msg_with_prefixes: String = with_prefix(record.args().to_string());
 
     let message = match level {
         Level::Error => msg_with_prefixes.red(),
@@ -27,8 +40,29 @@ pub fn custom_log_format(buf: &mut Formatter, record: &Record) -> std::io::Resul
 #[macro_export]
 macro_rules! success {
     ($($arg:tt)*) => {{
-        use colored::Colorize;
-        let message = format!($($arg)*).replace("\n", "\n |  ");
-        println!("{} {}", "[✔]".green(), message.green());
+        use $crate::custom_log::success_message;
+        let message = format!($($arg)*);
+        println!("{}", success_message(message));
     }};
+}
+
+pub struct OlSpinner {
+    spinner: Spinner,
+}
+
+impl OlSpinner {
+    pub fn new(message: &str) -> Self {
+        let spinner = Spinner::new(spinners::Aesthetic, message.to_owned(), Color::White);
+        OlSpinner { spinner }
+    }
+
+    pub fn stop_with_success(&mut self, message: &str) {
+        self.spinner
+            .stop_with_message(success_message(message.to_owned()).as_str());
+    }
+
+    pub fn stop_with_error(&mut self, message: &str) {
+        self.spinner
+            .stop_with_message(error_message(message.to_owned()).as_str());
+    }
 }
